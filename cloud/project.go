25b158cb941e83a27178e7b3412fa6a9ee730a8c
admdwrf <frederic.alix@fredalix.com>
2016-11-19 16:29:08 +0100

    adding cloud pkg

---
+++ cloud/project.go
@@ -0,0 +1,16 @@
+package cloud
+
+import "github.com/spf13/cobra"
+
+func init() {
+	cmdCloudProject.AddCommand(cmdProjectList)
+	cmdCloudProject.AddCommand(cmdProjectInfo)
+}
+
+// cmdCloudProject ...
+var cmdCloudProject = &cobra.Command{
+	Use:     "project",
+	Short:   "Project commands: ovhcli cloud project --help",
+	Long:    `Project commands: ovhcli cloud project <command>`,
+	Aliases: []string{"pr"},
+}
---
+++ cloud/projectinfoid.go
@@ -0,0 +1,37 @@
+package cloud
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/admdwrf/ovhcli/sdk"
+	"github.com/spf13/cobra"
+)
+
+var projectid string
+
+func init() {
+	cmdProjectInfo.PersistentFlags().StringVarP(&projectid, "projectid", "", "", "Your ID Project")
+}
+
+var cmdProjectInfo = &cobra.Command{
+	Use:   "infobyid",
+	Short: "Info about a project with the project ID: ovhcli cloud project infobyid",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		c, err := sdk.NewClient()
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+			os.Exit(1)
+		}
+
+		projectinfo, err := c.CloudProjectInfoByID(projectid)
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Project Name: %s\n", projectinfo.Name)
+		fmt.Printf("Project Status: %s\n", projectinfo.Status)
+		fmt.Printf("Creation Date: %s\n", projectinfo.CreationDate)
+	},
+}

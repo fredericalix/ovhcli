956824ef0d6f20f31c7034f8f84fe300679110b1
admdwrf <frederic.alix@fredalix.com>
2016-11-21 15:01:06 +0100

    Cloud: Add func instance info

---
+++ cloud/instanceinfo.go
@@ -0,0 +1,24 @@
+package cloud
+
+import (
+	"github.com/admdwrf/ovhcli/internal"
+	"github.com/spf13/cobra"
+)
+
+var instanceName string
+
+func init() {
+	cmdInstanceInfo.PersistentFlags().StringVarP(&instanceName, "instanceName", "", "", "Your Instance name")
+	cmdInstanceInfo.PersistentFlags().StringVarP(&projectID, "projectID", "", "", "Your ID Project")
+
+}
+
+var cmdInstanceInfo = &cobra.Command{
+	Use:   "info",
+	Short: "Info about an cloud instance: ovhcli cloud instance info",
+	Run: func(cmd *cobra.Command, args []string) {
+		instanceinfo, err := internal.Client.CloudInfoInstance(projectID, instanceName)
+		internal.Check(err)
+		internal.FormatOutputDef(instanceinfo)
+	},
+}

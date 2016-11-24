e817de338f06b8d9ce451cbaf60327cfb257a4a8
Cyrille Meichel <cyrille.meichel@corp.ovh.com>
2016-11-24 14:29:20 +0100

    use default client to connect

--- ovhcli/connect/cmd.go
+++ ovhcli/connect/cmd.go
@@ -10,12 +10,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var endpoint string
-
-func init() {
-	Cmd.PersistentFlags().StringVarP(&endpoint, "endpoint", "", "ovh-eu", "ovh-eu | kimsufi-eu | kimsufi-ca | soyoustart-eu | soyoustart-ca | runabove-ca")
-}
-
 // Cmd domain
 var Cmd = &cobra.Command{
 	Use:   "connect",
@@ -23,7 +17,7 @@ var Cmd = &cobra.Command{
 	Long:  `Domain commands: ovhcli connect <command>`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		client, err := govh.NewEndpointClient(endpoint)
+		client, err := govh.NewDefaultClient()
 		if err != nil {
 			err = fmt.Errorf("Error while creating OVH Client: %s\nYou need to create an application; please visite this page https://eu.api.ovh.com/createApp/ and create your $HOME/ovh.conf file\n\t[default]\n\t; general configuration: default endpoint\n\tendpoint=ovh-eu\n\n\t[ovh-eu]\n\t; configuration specific to 'ovh-eu' endpoint\n\tapplication_key=my_app_key", err)
 		}
